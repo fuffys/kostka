14e9acf7cb9a1a16dd3f4700d870851128c8b373
unknown <adamhajdik@seznam.cz>
2020-10-21 00:02:11 +0200

    Verze 1.5

--- js/game.js
+++ js/game.js
@@ -1,54 +1,44 @@
-var hody = [];
+const cube = document.getElementById("cube");
+const cube2 =document.getElementById("cube2");
+const result = document.getElementById("result");
+const game = document.getElementById("game");
+var j = 0;
+let hod = 1;
+let hody = [];
 
-document.getElementById('game').addEventListener('click',
-    function(){
-        hod();
-        console.log(hody);
+
+function headerChange(){
+    var Hcolor=document.getElementById("nadpis");
+    var txtcolor=["red","blue"];
+    Hcolor.style.color=txtcolor[j];
+    j=(j+1)%txtcolor.length;
+}
+    setInterval(headerChange,400);
+
+
+game.addEventListener("click", function(){
+    hod = Math.ceil(Math.random() * 6);
+    hod2 = Math.ceil(Math.random() * 6);
+    hody.push(hod);
+    hody.push(hod2);
+    cube.src = "img/kostka" + hod + ".png";
+    cube2.src = "img/kostka" + hod2 + ".png";
+    console.log("Alea iacta est...")
+    vypisStatistiky();
+})
+
+function suma(){
+    let sum = 0;
+    for (let i = 0; i < hody.length; i++){
+        sum += hody[i];
     }
-);
-
-function suma(cisla) {
-    var sum = 0;
-    cisla.forEach(function(value,index){
-        sum += value;
-    })
     return sum;
 }
 
-function maximum(cisla) {
-    var max = 1;
-    cisla.forEach(function(value,index){
-        if (value > max) max = value;
-    })
-    return max;
+function vypisStatistiky(){
+    result.innerHTML = `<p>Poslední hod: ${hod}</p>`;
+    result.innerHTML += `<p>Počet hodů: ${hody.length}</p>`;
+    result.innerHTML += `<p>Součet: ${suma()}</p>`;
+    result.innerHTML += `<p>Průměr: ${(suma() / hody.length).toFixed(2)}</p>`;
 }
 
-function minimum(cisla) {
-    var min = 6;
-    cisla.forEach(function(value,index){
-        if (value < min) min = value;
-    })
-    return min;
-}
-
-function average(sum, count) {
-    return (sum / count).toFixed(2);
-}
-
-function hod() {
-    var h = Math.ceil(Math.random() * 6);
-    hody.push(h);
-    document.getElementById('cube').src='img/kostka' + h + '.png';
-    document.getElementById('result').innerHTML = '<p>Hod: ' + h + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Počet hodů: ' + hody.length + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Součet hodů: ' + suma(hody) + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Průměr hodů: ' + average(suma(hody),hody.length) + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Nejvyšší hod: ' + maximum(hody) + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Nejvyšší hod: ' + minimum(hody) + '</p>';
-    return h;
-}
